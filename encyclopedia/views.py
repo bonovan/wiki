e35d903f9e156865093d0a18723fbe0c4c6b24f2
bonovan <donovandelmundo1@gmail.com>
2024-06-24 16:33:14 -0500

    markdown to html conversion

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,22 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown2
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, name):
+    page = util.get_entry(name)
+    if (page == None):
+        return
+    else:
+        html = markdown2.markdown_path(f"entries/{name}.md")
+
+        return render(request, "encyclopedia/entry.html", {
+            "html" : html
+        })
 
