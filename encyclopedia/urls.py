35060a6d19982b8049106a9430f47b6209948d9c
bonovan <donovandelmundo1@gmail.com>
2024-06-26 13:39:49 -0500

    connected path from new page to layout.html

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.entry, name="entry"),
-    path("new", views.new, name="new"),
+    path("new/", views.new, name="new"),
 ]
