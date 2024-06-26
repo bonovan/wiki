07fabf9fa707dc2cf6330fb69db755534641e968
bonovan <donovandelmundo1@gmail.com>
2024-06-26 14:18:54 -0500

    random page selector implemented

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:entry>", views.entry, name="entry"),
     path("new/", views.new, name="new"),
+    path("rand/", views.rand, name="rand")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from . import util
 
 import markdown2
-
+import random
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -18,9 +18,20 @@ def entry(request, entry):
         html = markdown2.markdown_path(f"entries/{entry}.md")
 
         return render(request, "encyclopedia/entry.html", {
-            "html" : html
+            "html" : html,
+            "title" : entry
         })
 
 def new(request):
     return render(request, "encyclopedia/new.html")
 
+def rand(request):
+    entries = util.list_entries()
+    entry = random.choice(entries)
+    
+    html = markdown2.markdown_path(f"entries/{entry}.md")
+
+    return render(request, "encyclopedia/entry.html", {
+        "html" : html,
+        "title" : entry
+    })
